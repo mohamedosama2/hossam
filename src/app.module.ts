5a40a28891aadf3640f5eded05a8cb057b38cc42
Ahmed Hossam <ahmedhossam114231@gmail.com>
2022-09-07 10:26:32 +0200

    hossam

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,12 +24,12 @@ import { GroupModule } from './group/group.module';
     UniversityModule,
     SubjectsModule,
     GroupModule,
-   /*  ChatModule, */
-/*     NotificationModule, */
+    /*  ChatModule, */
+    /*     NotificationModule, */
     // MessageQueueModule,
     // cacheOperationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
