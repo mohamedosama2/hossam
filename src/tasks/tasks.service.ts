3a09a8f56bb43eaf12e57842ebd0039a61a7c7aa
Ahmed Hossam <ahmedhossam114231@gmail.com>
2022-09-18 14:43:58 +0200

    task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -9,33 +9,34 @@ import { UpdateTaskDto } from './dto/update-task.dto';
 import { TaskRepository } from './task.repository';
 
 @Injectable()
-export class TasksService {
+export class TasksService
+{
   constructor(
     private readonly TaskRepository: TaskRepository,
     private readonly PaymentService: PaymentService,
     private readonly NotificationService: NotificationService,
-  ) {}
-  async create(createTaskDto: CreateTaskDto) {
+  ) { }
+  async create(createTaskDto: CreateTaskDto)
+  {
     const payment = createTaskDto.payment as CreatePaymentTaskDto;
     delete createTaskDto.payment;
     let task = await this.TaskRepository.create(createTaskDto);
-    if (payment) {
+    if (payment)
+    {
       await this.PaymentService.create({
         ...payment,
         task: task._id,
-<<<<<<< HEAD
         paymentType: PaymentType.REVENUSE
-=======
-        pymentType: PaymentType.REVENUSE,
->>>>>>> b179a7c55382824fd100ba96726317a56e00e7c5
       });
     }
     const taskGroup = await this.TaskRepository.findPopulatedTask(task._id);
     const tokens = [];
-    ((taskGroup.group as any).students as any[]).forEach(({ student }) => {
-   /*    console.log('St', student); */
-      student.pushTokens.forEach(({ deviceToken }) => {
-       /*  console.log('St2', deviceToken); */
+    ((taskGroup.group as any).students as any[]).forEach(({ student }) =>
+    {
+      /*    console.log('St', student); */
+      student.pushTokens.forEach(({ deviceToken }) =>
+      {
+        /*  console.log('St2', deviceToken); */
         tokens.push({
           deviceToken: deviceToken ? deviceToken : 'testing',
           _id: student._id,
@@ -43,7 +44,7 @@ export class TasksService {
       });
     });
 
-/*     console.log((taskGroup.group as any).students); */
+    /*     console.log((taskGroup.group as any).students); */
     await this.NotificationService.sendMany(
       {
         body: task.subject,
@@ -54,7 +55,8 @@ export class TasksService {
     return task;
   }
 
-  async findAll(FilterQueryOptionsTasks: FilterQueryOptionsTasks) {
+  async findAll(FilterQueryOptionsTasks: FilterQueryOptionsTasks)
+  {
     return await this.TaskRepository.findAllWithPaginationOption(
       FilterQueryOptionsTasks,
       ['university', 'subject', 'status'],
@@ -62,18 +64,21 @@ export class TasksService {
     );
   }
 
-  async findOne(_id: string) {
+  async findOne(_id: string)
+  {
     const isExisted = await this.TaskRepository.findOne({ _id });
     if (!isExisted) throw new NotFoundException();
     return isExisted;
   }
 
-  async update(_id: string, updateTaskDto: UpdateTaskDto) {
+  async update(_id: string, updateTaskDto: UpdateTaskDto)
+  {
     // await this.findOne(_id);
     return await this.TaskRepository.updateOne({ _id }, updateTaskDto);
   }
 
-  async deleteTask(id: string) {
+  async deleteTask(id: string)
+  {
     return await this.TaskRepository.deleteOne({ _id: id });
   }
 }
