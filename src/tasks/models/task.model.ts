0684534b483bf4d4563053193af081abeafde828
Ahmed Hossam <ahmed.hossam@qara.net>
2023-06-12 22:15:36 +0300

    tasl schema

--- src/tasks/models/task.model.ts
+++ src/tasks/models/task.model.ts
@@ -7,24 +7,81 @@ import { User } from 'src/users/models/_user.model';
 
 export type TaskDocument = Task & Document;
 
-export enum State
-{
+export enum State {
   PENDING = 'PENDING',
   ACTIVE = 'ACTIVE',
   COMPLETED = 'COMPLETED',
 }
-export class Manager
-{
+
+export enum TaskType {
+  GRADUATION = 'GRADUATION',
+  SINGLE = 'SINGLE',
+  PRIVATE = 'PRIVATE',
+}
+
+export enum TaskLevel {
+  PORPOSAL = 'PORPOSAL',
+  MIDTERM = 'MIDTERM',
+  FINAL = 'FINAL',
+  GRADE = 'GRADE',
+
+}
+
+export enum LevelType {
+  PORPOSAL = 'PORPOSAL',
+  MIDTERM = 'MIDTERM',
+  PERSENTATION = 'PERSENTATION',
+  REPORT = 'REPORT',
+  DEMO = 'DEMO',
+  TOTAL = 'TOTAL',
+}
+
+export enum Semester {
+  FIRST = 'FIRST',
+  SECOND = 'SECOND',
+}
+
+export enum ATTENDENCEPALCE {
+  ONSITE = 'ONSITE',
+  REMOTE = 'REMOTE',
+}
+export class Manager {
   @Prop({ type: String, required: true })
   id?: string;
 
   @Prop()
   name?: string;
 }
+export class Level {
+  @Prop({ type: String, enum: Object.values(LevelType), required: false })
+  levelType?: LevelType;
+
+  @Prop({ type: String, enum: Object.values(State), required: false })
+  levelStatus?: State;
+
+  @Prop()
+  name?: string;
+
+  @Prop({ type: Number, required: false })
+  degree?: number;
+
+  @Prop({ type: Date, required: false })
+  deuDate?: Date;
+}
+
+
+export class TaskLevels {
+
+
+  @Prop({ type: String, enum: Object.values(LevelType), required: false })
+  taskLevel?: LevelType;
+
+  @Prop({ type: () => Level, required: false })
+  taskLevelData?: Level[];
+}
 
 @Schema({ timestamps: true })
-export class Task
-{
+export class Task {
   id?: string;
 
   @Prop({
@@ -57,6 +114,26 @@ export class Task
   })
   group?: string;
 
+  @Prop({
+    type: MongooseSchema.Types.ObjectId,
+    ref: User.name,
+    required: false,
+  })
+  student?: string;
+
+  @Prop({
+    type: MongooseSchema.Types.ObjectId,
+    ref: User.name,
+    required: false,
+  })
+  programmer?: string;
+  @Prop({
+    type: MongooseSchema.Types.ObjectId,
+    ref: User.name,
+    // required: true,
+  })
+  reporter?: string;
+
   @Prop({ type: () => Manager })
   taskManager?: Manager;
 
@@ -69,10 +146,39 @@ export class Task
   @Prop({ type: String, enum: Object.values(State) })
   state?: State;
 
+  @Prop({ type: String, enum: Object.values(State) })
+  taskType?: State;
+
+  // single task
+  @Prop({ type: String, required: true, enum: Object.values(Semester) })
+  semester: Semester;
+
+  // private task
+
+  @Prop({ type: String, required: true, enum: Object.values(ATTENDENCEPALCE) })
+  attendancePlace: ATTENDENCEPALCE;
+
+
+  @Prop({ type: Number, required: true })
+  numberOfHours?: number;
+
+
+  @Prop({ type: Number, required: true })
+  pricePerHour?: number;
+
+
+  @Prop({ type: () => Level })
+  levels?: Level[];
+
+
   @Prop({ type: Date, required: true })
   startDate?: Date;
 
-  @Prop({ type: Date })
+  // ================== here question single task 
+  @Prop({ type: Date, required: true })
+  studyYear?: Date;
+
+  @Prop({ type: Date, required: false })
   deuDate?: Date;
 
   @Prop({ type: Date, required: true })
